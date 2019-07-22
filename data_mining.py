6cdbec6b608bbba711823c20999a4813c96059e1
Kryukich <belyakov0va@gmail.com>
2019-07-22 20:23:10 +0300

    really last

--- data_mining.py
+++ data_mining.py
@@ -14,14 +14,14 @@ data resource https://www.finam.ru/profile/moex-akcii/gazprom/export/
 link example: " http://export.finam.ru/GAZP_190722_190722.csv?
               market=1&em=16842&code=GAZP&apply=0&df=22&mf=6&yf=2019&from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&
               f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1 "
-"""                 
-#import requests
+"""
+import urllib.request
 
-''' example = requests.get('http://export.finam.ru/\
+file_link = 'http://export.finam.ru/\
 GAZP_190722_190722.csv?\
 market=1&em=16842&code=GAZP&apply=0&df=22&mf=6&yf=2019&\
 from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&\
 f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&\
-mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1')
+mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1'
 
-print(example.content)'''
+urllib.request.urlretrieve(file_link, '/Users/Belyakov.VA/Desktop/UKL19/DATA/test_file.csv')
