d2d14e6f3c6b2a676eba2ae827890b1897cf2139
Kryukich <belyakov0va@gmail.com>
2019-07-23 20:50:45 +0300

    last_today

--- data_mining.py
+++ data_mining.py
@@ -10,10 +10,7 @@ Blue chips in Russia
 7) МТС                 15) Норникель
 8) Сургутнефтегаз
 
-data resource https://www.finam.ru/profile/moex-akcii/gazprom/export/
-link example: " http://export.finam.ru/GAZP_190722_190722.csv?
-              market=1&em=16842&code=GAZP&apply=0&df=22&mf=6&yf=2019&from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&
-              f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1 "
+data resource https://www.finam.ru/profile/moex-akcii/company_name/export
 """
 import urllib
 
@@ -24,7 +21,7 @@ from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&\
 f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&\
 mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1'
 
-'http://export.finam.ru/SBER_190723_190723.txt?market=1&em=3&code=SBER&\ apply=0 &df=23&mf=6&yf=2019&from=23.07.2019&dt=23&mt=6&yt=2019&to=23.07.2019&p=7&f=SBER_190723_190723&e=.txt&cn=SBER&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1&at=1'
+'http://export.finam.ru/SBER_190723_190723.txt?market=1&em=3&code=SBER&apply=0&df=23&mf=6&yf=2019&from=23.07.2019&dt=23&mt=6&yt=2019&to=23.07.2019&p=7&f=SBER_190723_190723&e=.txt&cn=SBER&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1&at=1'
 
 urllib.request.urlretrieve(file_link, '/Users/Belyakov.VA/Desktop/UKL19/DATA\
 /test_file.csv')
