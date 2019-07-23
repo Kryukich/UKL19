80e7bc038513c5d00eb01cf5b2d07f5c6d5269fe
Kryukich <belyakov0va@gmail.com>
2019-07-23 19:49:49 +0300

    change_method: open=>write_content

--- data_mining.py
+++ data_mining.py
@@ -15,7 +15,7 @@ link example: " http://export.finam.ru/GAZP_190722_190722.csv?
               market=1&em=16842&code=GAZP&apply=0&df=22&mf=6&yf=2019&from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&
               f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1 "
 """
-import urllib.request
+import urllib
 
 file_link = 'http://export.finam.ru/\
 GAZP_190722_190722.csv?\
@@ -24,4 +24,11 @@ from=22.07.2019&dt=22&mt=6&yt=2019&to=22.07.2019&p=7&\
 f=GAZP_190722_190722&e=.csv&cn=GAZP&dtf=1&tmf=1&MSOR=1&\
 mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1'
 
-urllib.request.urlretrieve(file_link, '/Users/Belyakov.VA/Desktop/UKL19/DATA/test_file.csv')
+'http://export.finam.ru/SBER_190723_190723.txt?market=1&em=3&code=SBER&apply=0&df=23&mf=6&yf=2019&from=23.07.2019&dt=23&mt=6&yt=2019&to=23.07.2019&p=7&f=SBER_190723_190723&e=.txt&cn=SBER&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1&at=1'
+
+urllib.request.urlretrieve(file_link, '/Users/Belyakov.VA/Desktop/UKL19/DATA\
+/test_file.csv')
+f = open("company_quotes.txt", "w")
+content = page.read()
+f.write(content)
+f.close()
