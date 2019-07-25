dfb09e61fc0034510181876d98317edc12cd72a8
Kryukich <belyakov0va@gmail.com>
2019-07-26 00:43:14 +0300

    from home

--- 2_search_ID.py
+++ 2_search_ID.py
@@ -6,9 +6,11 @@ with open('for_em.txt', encoding='utf-8') as emitent_information:
     for line in emitent_information:
         data_list.append(line)
 
-_id, _name = data_list[0], data_list[1]
+_id, _name, _code = data_list[0], data_list[1], data_list[2] 
 _id = _id[_id.index('[') + 1: _id.index(']')].split(',')
 _name = _name[_name.index('[') + 1: _name.index(']')].split(',')
+_code = _code[_code.index('[') + 1: _code.index(']')].split(',')
 # for i in range():
-print(_id)
+print(_id, _code, _name)
+print(len(_id), len(_code))
 # print(_name)
