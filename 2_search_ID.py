95ace8f45d4d8551cab2c7efdc4e4e26c0e9878a
Kryukich <belyakov0va@gmail.com>
2019-07-30 16:59:13 +0300

    30.07.19

--- 2_search_ID.py
+++ 2_search_ID.py
@@ -6,11 +6,11 @@ with open('for_em.txt', encoding='utf-8') as emitent_information:
     for line in emitent_information:
         data_list.append(line)
 
-_id, _name, _code = data_list[0], data_list[1], data_list[2] 
+_id, _name, _code = data_list[0], data_list[1], data_list[2]
 _id = _id[_id.index('[') + 1: _id.index(']')].split(',')
-_name = _name[_name.index('[') + 1: _name.index(']')].split(',')
 _code = _code[_code.index('[') + 1: _code.index(']')].split(',')
-# for i in range():
-print(_id, _code, _name)
-print(len(_id), len(_code))
-# print(_name)
+# убираю кавычки у тикера
+for ticker in range(len(_code)):
+    _code[ticker] = _code[ticker][1: len(_code[ticker]) - 1]
+dictionary_of_ticker_and_id = dict(zip(_code, _id))  # объединяю в словарь
+print(len(_id), len(_code), 'SBER' in dictionary_of_ticker_and_id)
